51355e23c822fd6a63c419e117db8a53655ea5c0
KirillYrievich <afanas.lg2@gmail.com>
2023-08-09 02:57:36 +0300

    intermediate control work Python

---
+++ helper.py
@@ -0,0 +1,85 @@
+import json, time, os
+from datetime import datetime
+
+
+class Nout:
+    def __init__(self):
+        self.db = self.load_db()
+
+    def load_db(self):
+        return json.load(open('noute_base.json', encoding='utf-8'))
+
+    def add_nout(self):
+
+        print("Новая заметка")
+
+        result = {str(self.db["__service_tag__"]["count"]): {
+            "title": input("Введите название заметки: "),
+            "text": input("Введите текст заметки: "),
+            "create": datetime.now().strftime("%Y-%m-%d"),
+            "update": None}}
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f"Заметка  {result[list(result.keys())[0]]['title']} добавлена")
+        time.sleep(0.8)
+        self.db.update(result)
+        self.db["__service_tag__"]["count"]+=1
+
+    def save_db(self):
+        for i in range(4, 0, -1):
+            print(f"\rДанные сохраняются..{i}", end='')
+            time.sleep(0.33)
+        with open('noute_base.json', 'w', encoding='utf-8') as file:
+            json.dump(self.db, file, indent=4, ensure_ascii=False)
+
+
+    def print(self):
+        print(f"_____________________________\nID\t|Наименнование\n-----------------------------")
+        for nout in range(1, len(self.db)):
+            print(f"{nout}\t|{self.db[str(nout)]['title']}")
+        print(f"-----------------------------")
+
+    def read_nout(self, id):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        if self.db.get(str(id)) and id != "__service_tag__":
+            print(f"Заметка: {self.db[str(id)]['title']}\n-----------------------------\n{self.db[str(id)]['text']}\n\n"
+                  f"Заметка создана: {self.db[str(id)]['create']}\n"
+                  f"Заметка изменена: {self.db[str(id)]['update']}")
+            input("Для продолжения работы нажмите любую клавишу...")
+        else:
+            input("Данная заметка не обнаружена.\nДля продолжения работы нажмите любую клавишу...")
+
+    def del_nout(self, id):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        if self.db.get(str(id)) and id != "__service_tag__":
+            input(f"Заметка: {self.db[str(id)]['title']}, удалена!\nДля продолжения работы нажмите любую клавишу...")
+            del self.db[str(id)]
+        else:
+            input("Данная заметка не обнаружена.\nДля продолжения работы нажмите любую клавишу...")
+
+    def edit_nout(self, id):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        if self.db.get(str(id)) and id != "__service_tag__":
+            print(f"Заметка: {self.db[str(id)]['title']}\n-----------------------------")
+            num = input(f"Что вы хотите изменить?\n\n1. Изменить название\n2. Изменить текст\n\n"
+                        f"или введите любой символ для отмены\n\nВыберете пункт меню:  ")
+            if num in ['1', '2']:
+                match num:
+                    case '1':
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        print(f"Старое название заметки: {self.db[str(id)]['title']}\n-----------------------------")
+                        self.db[str(id)]['title'] = input("Введите новое название для заметки: ")
+                        self.db[str(id)]['update'] = datetime.now().strftime("%Y-%m-%d %H:%M")
+                    case '2':
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        print(f"Старый текст заметки:\n{self.db[str(id)]['text']}\n-----------------------------")
+                        self.db[str(id)]['text'] = input("Введите новый текст заметки: \n")
+                        self.db[str(id)]['update'] = datetime.now().strftime("%Y-%m-%d %H:%M")
+            else:
+                print("down")
+
+        else:
+            input("Данная заметка не обнаружена.\nДля продолжения работы нажмите любую клавишу...")
+
+if __name__ == '__main__':
+    n = Nout()
+    n.add_nout()
---
+++ menu.py
@@ -0,0 +1,101 @@
+import os
+from helper import Nout
+def menu():
+    n = Nout()
+    print("Привет ты в программе заметки в консоли")
+    work =True
+    while work:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print()
+        num, men = first_menu()
+        match num:
+            case 1:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                num, men = work_nout()
+                match num:
+                    case 1:
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        n.print()
+                        num, men = re_up_del_nout()
+                        match num:
+                            case 1:
+                                os.system('cls' if os.name == 'nt' else 'clear')
+                                n.print()
+                                n.read_nout(input("Выберкрите заметку (ID) :"))
+                            case 2:
+                                os.system('cls' if os.name == 'nt' else 'clear')
+                                n.print()
+                                n.edit_nout(input("Выберкрите заметку (ID) :"))
+                            case 3:
+                                os.system('cls' if os.name == 'nt' else 'clear')
+                                n.print()
+                                n.del_nout(input("Выберкрите заметку (ID) :"))
+                            case 0:
+                                os.system('cls' if os.name == 'nt' else 'clear')
+                                continue
+                    case 2:
+                        pass
+                    case 0:
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        continue
+            case 2:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                n.add_nout()
+            case 3:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                n.save_db()
+            case 0:
+                if n.db == n.load_db():
+                    work = False
+                else:
+                    while True:
+                        save_data = input("Есть не сохраненные данные, вы желаете их сохранить? [Д/н]:")
+                        if save_data == "Д":
+                            n.save_db()
+                            work = False
+                            break
+                        elif save_data == "н":
+                            work = False
+                            break
+
+def first_menu():
+    try:
+        ls = {1: "Работа с заметками", 2: "Добавить заметку", 3: "Сохранить все изменения", 0: "Выход"}
+        num = int(input(f"1. {ls[1]}\n"
+                        f"2. {ls[2]}\n"
+                        f"3. {ls[3]}\n"
+                        f"0. {ls[0]}\n"
+                        "Выберете пункт меню:  "))
+    except ValueError:
+        return -1, None
+    else:
+        return num, ls.get(num)
+
+def work_nout():
+    try:
+        ls = {1: "Показать список заметок", 2: "Показать список заметок с фильтрами", 0: "Выход"}
+        num = int(input(f"1. {ls[1]}\n"
+                        f"2. {ls[2]}\n"
+                        f"0. {ls[0]}\n"
+                        "Выберете пункт меню:  "))
+    except ValueError:
+        return -1, None
+    else:
+        return num, ls.get(num)
+
+def re_up_del_nout():
+    try:
+        ls = {1: "Читать заметку", 2: "Редактировать заметку", 3: "Удалить заметку", 0: "Выход"}
+        num = int(input(f"1. {ls[1]}\n"
+                        f"2. {ls[2]}\n"
+                        f"3. {ls[3]}\n"
+                        f"0. {ls[0]}\n"
+                        "Выберете пункт меню:  "))
+    except ValueError:
+        return -1, None
+    else:
+        return num, ls.get(num)
+
+
+if __name__ == "__main__":
+    menu()
